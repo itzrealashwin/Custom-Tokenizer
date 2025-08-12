4f7b3ff56cd7c046202823f8e9c7fe684ff68a39
Ashwin <maliashwin2005@gmail.com>
2025-08-12 18:27:30 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors")
+const cors = require("cors");
 const tokenRoutes = require("./routes/tokenRoutes");
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://custom-tokenizer-api-reference.vercel.app'
+];
+
 const app = express();
-app.use(cors())
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // Allow non-browser requests like curl, postman
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error('Not allowed by CORS'), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+}));
+
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api", tokenRoutes);
 
