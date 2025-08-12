8079dc61a6c55582f9dc41d48e0357b7a98204a8
Ashwin <maliashwin2005@gmail.com>
2025-08-12 18:16:39 +0530

    minor changes on cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors")
 const tokenRoutes = require("./routes/tokenRoutes");
 
 const app = express();
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api", tokenRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
