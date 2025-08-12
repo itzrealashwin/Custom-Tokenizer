27e900c00d69fc6fe707eb70db206627d4f91a70
Ashwin <maliashwin2005@gmail.com>
2025-08-12 19:38:28 +0530

    Fixed error in response

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const router = require("./routes/tokenRoutes");
 
 const app = express();
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    'https://custom-tokenizer-api-reference.vercel.app',
-    'http://localhost:3000'
-  ];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204); // respond OK to preflight
-  }
-
-  next();
-});
+app.use(cors())
 
 app.use(express.json());
 
-app.post('/api/encode', (req, res) => {
-  // Your encoding logic here
-  res.json({ message: "Encode API working!" });
-});
+app.use("/api", router)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
