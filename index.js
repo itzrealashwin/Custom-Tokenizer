96b9e778f498c11d15fc5ce2516f2525a3943d97
Ashwin <maliashwin2005@gmail.com>
2025-08-12 19:41:47 +0530

    Added Health Route

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes/tokenRoutes");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 app.use(cors())
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use("/health", (req, res) => {
+  res.send("Good");
+});
 app.use("/api", router)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
